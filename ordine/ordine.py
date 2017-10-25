0319759d9a5258274849ab0e8c097963aafa361b
lk235 <cool.lk235@icloud.com>
2017-10-25 11:36:44 +0200

    10.25 day

--- ordine/ordine.py
+++ ordine/ordine.py
@@ -6,6 +6,14 @@ import os, sys
 # sys.setdefaultencoding("utf-8")
 import csv
 import pandas as pd
+from datetime import datetime
+
+def replace_dot(s):
+    s = s.replace('.',':') + ':00'
+    # time_result = datetime.strptime(s, "%d/%m/%Y %H:%M:%S")
+    # print time_result
+    return s
+    # return s.replace('.',':') + ':00'
 
 input_file = 'dailyshop di Chen luca.xlsx'
 
@@ -14,9 +22,10 @@ barcode_table = pd.read_csv('barcode.txt')
 Esempio_ordine = pd.read_csv('_Esempio_ordine.txt',sep=';')
 print Esempio_ordine.keys()
 
-input_df = pd.read_excel(input_file)
+input_df = pd.read_excel(input_file,index_col=None)
 output_df = input_df
 barcode_title = unicode('商品条码', "utf8", errors="ignore")
+order_date_title = unicode('订单时间', "utf8", errors="ignore")
 # print input_df[barcode_title]
 
 
@@ -24,9 +33,36 @@ Esempio_ordine_new = pd.DataFrame(data=None,columns=Esempio_ordine.columns)
 
 Esempio_ordine_new['cod_articolo'] = input_df[barcode_title]
 
-
+index_len = len(Esempio_ordine_new.index)
+Esempio_ordine_new['version'] = ['1.0']*index_len
+Esempio_ordine_new['ID_cliente'] = ['4E4B484E434D']* index_len
+Esempio_ordine_new['codice_cliente'] = ['013428'] * index_len
+Esempio_ordine_new['cod_articolo'] = input_df[barcode_title]
+# Esempio_ordine_new['nr_ordine'] =
+Esempio_ordine_new['data_ordine'] = input_df[order_date_title]
+Esempio_ordine_new['data_ordine'] = Esempio_ordine_new['data_ordine'].apply(replace_dot)
 for index, row in Esempio_ordine_new.iterrows():
     print index
-    row['cod_articolo'] = 1
+    row['cod_articolo'] = 00000000000
 
-print Esempio_ordine_new
+print Esempio_ordine_new.index
+# Esempio_ordine_new['data_ordine'].astype('datetime64')
+# Esempio_ordine_new['data_ordine'] = pd.to_datetime(Esempio_ordine_new['data_ordine'],format="%d/%m/%Y %H:%M:%S")
+print Esempio_ordine_new['data_ordine']
+# print Esempio_ordine_new['cod_articolo']
+
+# test =  pd.to_datetime(['08/10/2017'])
+# tm = pd.Timestamp('24/10/2017')
+# print tm
+# # tm.map(lambda tm: tm.strftime('%d-%m-%Y'))
+# tm1 =  tm.strftime("%d/%m/%y %H:%M:%S")
+# print type(tm1)
+
+# tm2 =  datetime.strptime('23/9/2017 00:00:00',"%d/%m/%Y %H:%M:%S")
+# print tm2
+# print type(tm2)
+# dt = datetime.strptime("21/11/06 16:30", "%d/%m/%y %H:%M")
+# print dt
+
+Esempio_ordine_new.to_csv('test.csv',sep=':')
+
